c77e17143739836e63e121d06a7c5ba846390da9
harryprayiv <harryprayiv@gmail.com>
2026-02-21 15:42:53 -0500

    remove codegen on this branch (refer back later if needed)

--- frontend/src/Codegen/Run.purs
+++
@@ -1,9 +0,0 @@
-module Codegen.Run where
-
-import Prelude
-import Codegen.Main (runCodegen)
-import Effect (Effect)
-import Schemas.Dispensary (dispensarySchema)
-
-main :: Effect Unit
-main = runCodegen dispensarySchema
